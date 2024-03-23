17ac2bcdc291dcc1068ae47099084593eea03f6b
VasuG19 <99899682+VasuG19@users.noreply.github.com>
2024-03-23 23:27:17 +0000

    fixes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
   import HelloWorld from './components/HelloWorld.vue'
   import Menu from './components/Mobile_nav.vue'
-  import NavLinks from './components/NavLinks.vue'
 </script>
 
 
