6b6e9d9158e2ebd7b35757f914c0cef78b530e91
Vasu <99899682+VasuG19@users.noreply.github.com>
2024-03-24 22:09:22 +0000

    updates

--- src/views/Office.vue
+++ src/views/Office.vue
@@ -22,7 +22,7 @@ export default {
 
       // Camera
       camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-      camera.position.set(0, 0, 5);
+      camera.position.set(0, 0, 3);
 
       // Renderer
       renderer = new THREE.WebGLRenderer();
